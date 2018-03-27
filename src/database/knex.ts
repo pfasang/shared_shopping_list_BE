e72e3dc2729e8a752332e6e0732952c4327c6af6
Patrik Fašang <patrik.fasang@gmail.com>
2018-03-27 17:35:40 +0200

    [FIX] knex definition fix

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -3,6 +3,9 @@ import * as knexFile from './Knexfile';
 
 let Knex: knex;
 switch (process.env.NODE_ENV) {
+    case 'production':
+        Knex = knex(knexFile.production);
+        break;
     default:
         Knex = knex(knexFile.development);
         break;
