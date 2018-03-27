999dfb89a4b9689cdf1c76fc6da8565f5a379915
Patrik Fašang <patrik.fasang@gmail.com>
2018-03-27 15:19:51 +0200

    [CONFIG] production database config

--- src/database/Knexfile.ts
+++ src/database/Knexfile.ts
@@ -15,6 +15,17 @@ let knexx = {
         seeds: {
             directory: __dirname + '/seeds/dev'
         }
+    },
+    production: {
+        client: 'pg',
+        connection: "postgres://ecrgafqgzfehjf:94b25422432a68d44bbd1f539feabdc67efe7d883e16812ccfeb9430096733e7@ec2-107-20-151-189.compute-1.amazonaws.com:5432/d84noukdb69965",
+        migrations: {
+            directory: __dirname + '/migrations'
+        },
+        debug: false,
+        seeds: {
+            directory: __dirname + '/seeds/dev'
+        }
     }
 };
 
